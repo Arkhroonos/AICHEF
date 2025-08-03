87dd3ab39fbc4b62c43a1b19e17846bff8c93ee5
Khroonos <shoka.khr@gmail.com>
2025-08-03 19:29:09 +0200

    Created Ingredients-form component

--- apps/AICHEF/src/app/app.routes.ts
+++ apps/AICHEF/src/app/app.routes.ts
@@ -1,3 +1,17 @@
+/**
+ * Angular Imports
+ */
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+/**
+ * Internal Imports
+ */
+import { IngredientsFormComponent } from './ingredients-form/ingredients-form.component';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: IngredientsFormComponent,
+    title: 'Ingredients Form',
+  },
+];
---
+++ apps/AICHEF/src/app/ingredients-form/ingredients-form.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Angular Imports
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './ingredients-form.component.html',
+  styleUrl: './ingredients-form.component.scss',
+})
+export class IngredientsFormComponent {}
