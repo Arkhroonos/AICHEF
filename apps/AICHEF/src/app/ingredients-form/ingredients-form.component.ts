eac5e70f02a7f0770cbcdb266c45229eed5d2c1d
Khroonos <shoka.khr@gmail.com>
2025-08-18 16:05:22 +0200

    Added Possibility to add or remove

--- apps/AICHEF/src/app/ingredients-form/ingredients-form.component.ts
+++ apps/AICHEF/src/app/ingredients-form/ingredients-form.component.ts
@@ -1,10 +1,47 @@
 /**
  * Angular Imports
  */
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  FormArray
+} from '@angular/forms';
 
 @Component({
+  standalone: true,
   templateUrl: './ingredients-form.component.html',
-  styleUrl: './ingredients-form.component.scss',
+  styleUrls: ['./ingredients-form.component.scss'],
+  imports: [ReactiveFormsModule]
 })
-export class IngredientsFormComponent {}
+export class IngredientsFormComponent {
+  private readonly formBuilder: NonNullableFormBuilder = inject(
+    NonNullableFormBuilder
+  );
+
+  protected readonly ingredientForm = this.formBuilder.group({
+    ingredients: this.formBuilder.array([
+      this.formBuilder.group({
+        ingredient: [''],
+        quantity: ['']
+      })
+    ])
+  });
+
+  get ingredients() {
+    return this.ingredientForm.get('ingredients') as FormArray;
+  }
+
+  protected addIngredient() {
+    this.ingredients.push(
+      this.formBuilder.group({
+        ingredient: [''],
+        quantity: ['']
+      })
+    );
+  }
+
+  protected removeIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+}
