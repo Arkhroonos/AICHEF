bfc84af0e10097f8d0df36471a364e3e6d214e46
Khroonos <shoka.khr@gmail.com>
2025-08-24 17:37:11 +0200

    Completed Autocompletion
    created a function for filter
    Added an onSubmit event on the start Button.

--- apps/AICHEF/src/app/ingredients-form/ingredients-form.component.ts
+++ apps/AICHEF/src/app/ingredients-form/ingredients-form.component.ts
@@ -1,27 +1,53 @@
 /**
  * Angular Imports
  */
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit
+} from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import {
   NonNullableFormBuilder,
   ReactiveFormsModule,
-  FormArray
+  FormArray,
+  Validators
 } from '@angular/forms';
-import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
-import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from '@angular/material/autocomplete';
+import {
+  MatFormField,
+  MatInput,
+  MatLabel
+} from '@angular/material/input';
+import {
+  MatAutocomplete,
+  MatAutocompleteTrigger,
+  MatOption
+} from '@angular/material/autocomplete';
 
 /**
  * 3rd-party Imports
  */
-import { map, Observable, startWith, tap } from 'rxjs';
+import {
+  map,
+  Observable,
+  startWith
+} from 'rxjs';
 
 
 @Component({
   standalone: true,
   templateUrl: './ingredients-form.component.html',
   styleUrls: ['./ingredients-form.component.scss'],
-  imports: [ReactiveFormsModule, MatInput, MatAutocomplete, MatOption, MatAutocompleteTrigger, MatFormField, MatLabel, AsyncPipe]
+  imports: [
+    ReactiveFormsModule,
+    MatInput,
+    MatAutocomplete,
+    MatOption,
+    MatAutocompleteTrigger,
+    MatFormField,
+    MatLabel,
+    AsyncPipe
+  ]
 })
 export class IngredientsFormComponent implements OnInit {
   private readonly formBuilder: NonNullableFormBuilder = inject(
@@ -32,28 +58,38 @@ export class IngredientsFormComponent implements OnInit {
     ingredients: this.formBuilder.array(
       [
         this.formBuilder.group({
-          ingredient: [''],
-          quantity: ['']
+          ingredient: [
+            '',
+            Validators.required
+          ],
+          quantity: [
+            '',
+            Validators.required
+          ]
         })
       ]
     )
   });
 
-  protected filteredIngredients!: Observable<string[]>;
 
-  protected availableIngredients = ['Apple', 'Banana', 'Orange', 'Mango'];
+  protected availableIngredients = [
+    'Apple',
+    'Banana',
+    'Orange',
+    'Mango'
+  ];
+
+  protected createdFilters: Observable<string[]>[] = [];
 
   get ingredients() {
     return this.ingredientForm.get('ingredients') as FormArray;
   }
 
+
   ngOnInit() {
-    this.filteredIngredients = this.ingredients.at(0).get('ingredient')!.valueChanges.pipe(
-      startWith(''),
-      map((value: string) => this.filter(value || '')),
-      tap(value => console.log(value))
-    );
+    this.createdFilters.push(this.createFilter(0));
   }
+
 
   private filter(value: string): string[] {
     const filterValue = value.toLowerCase();
@@ -61,16 +97,38 @@ export class IngredientsFormComponent implements OnInit {
     return this.availableIngredients.filter(option => option.toLowerCase().includes(filterValue));
   }
 
+  private createFilter(index: number): Observable<string[]> {
+    return this.ingredients.at(index).get('ingredient')!.valueChanges.pipe(
+      startWith(''),
+      map((value: string) => this.filter(value || ''))
+    );
+  }
+
   protected addIngredient() {
     this.ingredients.push(
       this.formBuilder.group({
-        ingredient: [''],
-        quantity: ['']
+        ingredient: [
+          '',
+          Validators.required
+        ],
+        quantity: [
+          '',
+          Validators.required
+        ]
       })
     );
+    this.createdFilters.push(this.createFilter(this.ingredients.length - 1));
   }
 
   protected removeIngredient(index: number) {
     this.ingredients.removeAt(index);
+    this.createdFilters.splice(index, 1);
   }
+
+  protected onSubmit() {
+    if (this.ingredientForm.valid) {
+      console.log(this.ingredientForm.value);
+    }
+  }
+
 }
